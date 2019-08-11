aed23424d0a52ae9441800ba2bc413d540e32a3d
Aneta <atena771@wp.pl>
2019-08-11 13:26:40 +0000

    connect flask to database - mongodb

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] ='cook_book'
+app.config["MONGO_URI"] = 'mongodb+srv://AnetA:AnetA2@myfirstcluster-sfmlq.mongodb.net/cook_book?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
+
 @app.route('/')
-def hello():
-    return "Hello world"
+@app.route('/get_recipes')
+def get_recipes():
+  return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
     
 if __name__ == '__main__':
   app.run(host=os.environ.get('IP'),
