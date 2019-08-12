9563b90c5b0da86a428300b371ac96fba1959bda
Aneta <atena771@wp.pl>
2019-08-12 21:24:24 +0000

    add folders and templates to the project

--- app.py
+++ app.py
@@ -12,10 +12,35 @@ mongo = PyMongo(app)
 
 
 
+
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
   return render_template("recipes.html", recipes=mongo.db.recipes.find())
+  
+  
+@app.route('/add_recipe')
+def add_recipe():
+  return render_template('addrecipe.html', categories=mongo.db.categories.find())
+  
+  
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+  recipes=mongo.db.recipes
+  recipes.insert_one(request.form.to_dict())
+  return redirect(url_for('get_recipes'))
+  
+  
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+  the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+  all_categories = mongo.db.categories.find()
+  return render_template('viewrecipe.html', recipe=the_recipe,
+                           categories=all_categories)
+  
+
+  
+
 
     
 if __name__ == '__main__':
