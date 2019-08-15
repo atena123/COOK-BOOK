102b107ea0d9cf66c0f41592a821d8a11a6971a2
Aneta <atena771@wp.pl>
2019-08-15 19:22:56 +0000

    create editcategory.html with a content and create 'update' function

--- app.py
+++ app.py
@@ -72,12 +72,28 @@ def update_recipe(recipe_id):
 def delete_recipe(recipe_id):
   mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
   return redirect(url_for('get_recipes'))
+  
+  
 
 #-----------------------------------Categories-------------------
+
+
 
 @app.route('/get_categories')
 def get_categories():
   return render_template('categories.html', categories=mongo.db.categories.find())
+  
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+  return render_template('editcategory.html', category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+  
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+  mongo.db.categories.update({'_id': ObjectId(category_id)}, {'category_name': request.form.get('category_name')})
+  return redirect(url_for('get_categories'))
+  
+
+
 
 if __name__ == '__main__':
   app.run(host=os.environ.get('IP'),
